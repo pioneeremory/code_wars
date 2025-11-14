a85e31947089992af2c6e74a6933abfc24e6f35f
logan <blacksuitphoto@gmail.com>
2025-11-14 11:19:51 -0800

    first commit

---
+++ create_phone_number.py
@@ -0,0 +1,10 @@
+def create_phone_number(n):
+    #your code here
+    # => returns "(123) 456-7890"
+    # iterate through list and append each number to string output
+    # format output       
+                                                          
+   return (f'({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}')
+
+
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
